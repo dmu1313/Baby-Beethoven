57e6b336e2c5543a6de3ee5f551f50b1a66d4930
tokiron <31706950+tokiron@users.noreply.github.com>
2020-04-17 22:30:54 -0400

    MidiConv

---
+++ testMusicProcessing.py
@@ -0,0 +1,135 @@
+#Import Libraries ----------------------------------------
+#import torch
+#import torchvision
+from music21 import * 
+import numpy as np
+import os
+
+
+#Parameters-----------------------------------------------
+path = '/Users/Hannah Kim/Desktop/ESE587/FinalProject/MidiFiles/'
+sequence_length = 100
+
+
+#Method for reading MIDI files with music21---------------
+#writes notes and chords to an array
+def readMIDIfile(filePath):
+    print("Reading MIDI files!")
+    
+    notes=[]
+    
+    #For every file in folder
+    for file in os.listdir(filePath):
+        if file.endswith(".mid"):
+            print("Parsing %s" % filePath+file)
+            
+            #Open MIDI file
+            midiFile = converter.parse(filePath+file)
+            notes_to_parse = None
+            
+            p = instrument.partitionByInstrument(midiFile)
+            
+            for part in p.parts:
+                #Only extract Piano
+                notes_to_parse = part.recurse()
+           
+                for element in notes_to_parse:
+                    if isinstance(element, note.Note):
+                        notes.append(str(element.pitch))
+                    elif isinstance(element, chord.Chord):
+                        notes.append('.'.join(str(n) for n in element.normalOrder))
+                
+    return notes
+    
+#copied from Skuldur:------------------------------------
+#creates arrays for network input/output and maps notes to numbers
+def prepareData(notes, length):
+
+    # get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+    
+    # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+    
+    
+    network_input = []
+    network_output = []
+    
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+        
+    n_patterns = len(network_input)
+    
+    # reshape the input into a format compatible with LSTM layers
+    network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
+    
+    # normalize input
+   # network_input = network_input / float(length)
+    
+    #one hot encode output, uses Keras??
+    #network_output = np_utils.to_categorical(network_output)
+    
+    return (network_input, network_output)
+    
+
+notes = readMIDIfile(path)
+notes_len = len(set(notes))
+network_in, network_out = prepareData(notes, notes_len)
+
+print("This is the input array: \n", network_in)
+#print("input size:", network_in.shape)
+#print("output size:", len(network_out))
+
+print("\n\nThis is the output array: \n" , network_out)
+
+
+
+#copied from Skuldur:-------------------------------------
+#C
+def backToMIDI(network_output):
+    offset = 0
+    output_notes = []
+    
+    #Convert to notes
+    
+    
+    #Create MIDI file
+
+    for pattern in network_output:
+        # pattern is a chord
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = []
+            for current_note in notes_in_chord:
+                new_note = note.Note(int(current_note))
+                new_note.storedInstrument = instrument.Piano()
+                notes.append(new_note)
+            new_chord = chord.Chord(notes)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+        # pattern is a note
+        else:
+            new_note = note.Note(pattern)
+            new_note.offset = offset
+            new_note.storedInstrument = instrument.Piano()
+            output_notes.append(new_note)
+
+        # increase offset each iteration so that notes do not stack
+        offset += 0.5
+
+    midi_stream = stream.Stream(output_notes)
+
+    midi_stream.write('midi', fp='test_output.mid')
+
+
+
+
+
+
+
+
