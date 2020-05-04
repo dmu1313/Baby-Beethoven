cd03232ff4e77ddae72070d1429f58344f015a7a
Daniel Mu <dmu1313@gmail.com>
2020-05-04 12:22:42 -0400

    Completed hadamard and its test bench.

--- hardware/hadamard.sv
+++ hardware/hadamard.sv
@@ -1,22 +1,276 @@
 
-
 module hadamard
 #(
-    FLOAT_WIDTH = 31,
-    LOG_FLOAT_WIDTH = 5
+    FP_WIDTH = 32,
+    BRAM_WIDTH = 32,
+    WORD_BYTES = 4,
+    ADDR_WIDTH = 12
 )
 (
     input clk,
-    input [FLOAT_WIDTH-1:0] x,
-
-    output [FLOAT_WIDTH-1:0] y
+    input reset,
+    
+    input  [31:0] ps_control,
+    output [31:0] pl_status,
+    
+    output [ADDR_WIDTH-1:0] bram_addr_a,
+    input  [BRAM_WIDTH-1:0] bram_rddata_a,
+    output [BRAM_WIDTH-1:0] bram_wrdata_a,
+    output [WORD_BYTES-1:0] bram_we_a,
+    
+    output [ADDR_WIDTH-1:0] bram_addr_b,
+    input  [BRAM_WIDTH-1:0] bram_rddata_b,
+    output [BRAM_WIDTH-1:0] bram_wrdata_b,
+    output [WORD_BYTES-1:0] bram_we_b,
+    
+    output [ADDR_WIDTH-1:0] bram_addr_product,
+    input  [BRAM_WIDTH-1:0] bram_rddata_product,
+    output [BRAM_WIDTH-1:0] bram_wrdata_product,
+    output [WORD_BYTES-1:0] bram_we_product,
+    output mult_out_valid
 );
-
+    logic doneA, doneB;
+    logic incA, incB;
+    logic clearA, clearB;
+    logic first_half;
     
+    assign bram_we_a = 0;
+    assign bram_we_b = 0;
+    assign bram_wrdata_a = 32'bxxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx;
+    assign bram_wrdata_b = 32'bxxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx_xxxx;
+
+    had_datapath d(
+        .clk,
+        .reset,
+        
+        .bram_addr_a,
+        .bram_rddata_a,
+        
+        .bram_addr_b,
+        .bram_rddata_b,
+                
+        .bram_addr_product,
+        .bram_wrdata_product,
+        
+        .doneA,
+        .doneB,
+        .first_half,
+            
+        .incA,
+        .incB,
+        .clearA,
+        .clearB,
+        
+        .mult_out_valid
+    );
     
+    had_control c(
+        .clk,
+        .reset,
+        
+        .ps_control,
+        .pl_status,
+        
+        .bram_we_product,
+        
+        .doneA,
+        .doneB,
+        .first_half,
+        
+        .incA,
+        .incB,
+        .clearA,
+        .clearB
+    );
+endmodule
 
+module had_datapath
+#(
+    FP_WIDTH = 32,
+    BRAM_WIDTH = 32,
+    WORD_BYTES = 4,
+    ADDR_WIDTH = 12
+)
+(
+    input clk,
+    input reset,
     
+    output [ADDR_WIDTH-1:0] bram_addr_a,
+    input  [BRAM_WIDTH-1:0] bram_rddata_a,
+    
+    output [ADDR_WIDTH-1:0] bram_addr_b,
+    input  [BRAM_WIDTH-1:0] bram_rddata_b,
+    
+    output [ADDR_WIDTH-1:0] bram_addr_product,
+    output [BRAM_WIDTH-1:0] bram_wrdata_product,
+    
+    output doneA,
+    output doneB,
+    input first_half,
+    
+    input incA,
+    input incB,
+    input clearA,
+    input clearB,
+    
+    output mult_out_valid
+);
+    logic [ADDR_WIDTH-1:0]  counterA;
+    logic [ADDR_WIDTH-1:0]  counterB;
+    logic [ADDR_WIDTH-1:0]  addr;
+    logic [FP_WIDTH-1:0]    mult_out;
+//    logic mult_out_valid;
+    logic [ADDR_WIDTH-1:0] wr_addr;
+    
+    assign doneA = counterA == 2044;
+    assign doneB = counterB == 4092;
+    
+    assign addr = first_half ? counterA : counterB;
+    assign bram_addr_a = addr;
+    assign bram_addr_b = addr;
 
+    assign bram_addr_product = wr_addr;
+    assign bram_wrdata_product = mult_out;
 
+//    assign mult_out = bram_rddata_a * bram_rddata_b;
+    had_fp_mult your_instance_name (
+      .s_axis_a_tvalid(1),            // input wire s_axis_a_tvalid
+      .s_axis_a_tdata(bram_rddata_a),              // input wire [31 : 0] s_axis_a_tdata
+      .s_axis_b_tvalid(1),            // input wire s_axis_b_tvalid
+      .s_axis_b_tdata(bram_rddata_b),              // input wire [31 : 0] s_axis_b_tdata
+      .m_axis_result_tvalid(mult_out_valid),  // output wire m_axis_result_tvalid
+      .m_axis_result_tdata(mult_out)    // output wire [31 : 0] m_axis_result_tdata
+    );
+    
+    always_ff @(posedge clk) begin
+        if (reset)
+            wr_addr <= 0;
+        else
+            wr_addr <= addr;
+    
+        if (clearA)
+            counterA <= 0;
+        else if (incA)
+            counterA <= counterA + 4;
+        
+        if (clearB)
+            counterB <= 2048;
+        else if (incB)
+            counterB <= counterB + 4;
+    end
 
+endmodule
+
+typedef enum bit[3:0] {
+    WAIT_TO_START   = 0,
+    START_A         = 1,
+    LOOP_A          = 2,
+    DONE_A          = 3,
+    START_B         = 4,
+    LOOP_B          = 5,
+    DONE_B          = 6
+} hadamard_states;
+
+module had_control
+#(
+    BRAM_WIDTH = 32,
+    WORD_BYTES = 4,
+    ADDR_WIDTH = 12
+)
+(
+    input clk,
+    input reset,
+    
+    input  [31:0] ps_control,
+    output [31:0] pl_status,
+    
+    output [WORD_BYTES-1:0] bram_we_product,
+    
+    input  doneA,
+    input  doneB,
+    output first_half,
+    
+    output incA,
+    output incB,
+    output clearA,
+    output clearB
+);
+    logic [3:0] state;
+    logic [3:0] next_state;
+    logic completedA, completedB;
+    
+    assign pl_status[0] = completedA;
+    assign pl_status[1] = completedB;
+    
+    assign incA = state == START_A || state == LOOP_A;
+    assign incB = state == START_B || state == LOOP_B;
+    
+    assign clearA = state == WAIT_TO_START;
+    assign clearB = state == WAIT_TO_START;
+
+    assign first_half = (state == START_A || state == LOOP_A || state == DONE_A) ? 1 : 0;
+    
+    assign bram_we_product = (state == LOOP_A || state == DONE_A || state == LOOP_B || state == DONE_B) ? 4'b1111 : 4'b0000;
+
+    always_ff @(posedge clk) begin
+        if (reset)
+            completedA <= 0;
+        else if (state == DONE_A)
+            completedA <= 1;
+        else if (completedA == 1 && ps_control[0] == 0)
+            completedA <= 0;
+            
+        if (reset)
+            completedB <= 0;
+        else if (state == DONE_B)
+            completedB <= 1;
+        else if (completedB == 1 && ps_control[1] == 0)
+            completedB <= 0;
+    end
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            state <= WAIT_TO_START;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    always_comb begin
+        if (state == WAIT_TO_START) begin
+            if (ps_control[0] && completedA == 0)
+                next_state = START_A;
+            else if (ps_control[1] && completedB == 0)
+                next_state = START_B;
+            else
+                next_state = WAIT_TO_START;
+        end
+        
+        else if (state == START_A) begin
+            next_state = LOOP_A;
+        end
+        else if (state == LOOP_A) begin
+            if (doneA)
+                next_state = DONE_A;
+            else
+                next_state = LOOP_A;
+        end
+        else if (state == DONE_A) begin
+            next_state = WAIT_TO_START;
+        end
+        
+        else if (state == START_B) begin
+            next_state = LOOP_B;
+        end
+        else if (state == LOOP_B) begin
+            if (doneB)
+                next_state = DONE_B;
+            else
+                next_state = LOOP_B;
+        end
+        else if (state == DONE_B) begin
+            next_state = WAIT_TO_START;
+        end        
+    end
 endmodule
--- hardware/tb/hadamard_tb.sv
+++ hardware/tb/hadamard_tb.sv
@@ -6,94 +6,148 @@ module hadamard_tb
     WORD_BYTES = 4,
     ADDR_WIDTH = 12
 )();
-    logic executing;
-
+    logic executing; // Use this to differentiate between when we are loading from PS vs executing on PL
+    
+    logic mult_out_valid;
+    
     logic clk;
     logic reset;
     logic [31:0] ps_control, pl_status;
-
+    
+    // Signals that are actually used by the BRAMs.
     logic [ADDR_WIDTH-1:0] addr_a, addr_b, addr_product;
     logic [BRAM_WIDTH-1:0] rddata_a, rddata_b, rddata_product;
     logic [BRAM_WIDTH-1:0] wrdata_a, wrdata_b, wrdata_product;
     logic [WORD_BYTES-1:0] we_a, we_b, we_product;
+    
+    // Signals that our simulated PS can use to interact with the BRAMs.
+    logic [ADDR_WIDTH-1:0] ps_addr_a, ps_addr_b, ps_addr_product;
+    logic [BRAM_WIDTH-1:0] ps_wrdata_a, ps_wrdata_b, ps_wrdata_product;
+    logic [WORD_BYTES-1:0] ps_we_a, ps_we_b, ps_we_product;
+    
+    // Signals for the PL to use to interact with the BRAMs
+    logic [ADDR_WIDTH-1:0] pl_addr_a, pl_addr_b, pl_addr_product;
+    logic [BRAM_WIDTH-1:0] pl_wrdata_a, pl_wrdata_b, pl_wrdata_product;
+    logic [WORD_BYTES-1:0] pl_we_a, pl_we_b, pl_we_product;
+    
+    // Start Multiplexers /////////////////////////////////////////////////////////
+    assign addr_a = executing ? pl_addr_a : ps_addr_a;
+    assign addr_b = executing ? pl_addr_b : ps_addr_b;
+    assign addr_product = executing ? pl_addr_product : ps_addr_product;
+    
+    assign wrdata_a = executing ? pl_wrdata_a : ps_wrdata_a;
+    assign wrdata_b = executing ? pl_wrdata_b : ps_wrdata_b;
+    assign wrdata_product = executing ? pl_wrdata_product : ps_wrdata_product;
 
-    bram a0 #(
-        .BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH)
-    ) (
+    assign we_a = executing ? pl_we_a : ps_we_a;
+    assign we_b = executing ? pl_we_b : ps_we_b;
+    assign we_product = executing ? pl_we_product : ps_we_product;
+    // Stop Multiplexers //////////////////////////////////////////////////////////
+    
+
+    bram #(.BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH))
+    a0 (
         .clk, .reset,
         .bram_addr(addr_a), .bram_rddata(rddata_a),
         .bram_wrdata(wrdata_a), .bram_we(we_a)
     );
 
-    bram b0 #(
-        .BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH)
-    ) (
+    bram #(.BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH))
+    b0 (
         .clk, .reset,
         .bram_addr(addr_b), .bram_rddata(rddata_b),
         .bram_wrdata(wrdata_b), .bram_we(we_b)
     );
 
-    bram product #(
-        .BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH)
-    ) (
+    bram #(.BRAM_WIDTH(BRAM_WIDTH), .BRAM_SIZE(4096), .WORD_SIZE(WORD_BYTES), .ADDR_WIDTH(ADDR_WIDTH))
+    product (
         .clk, .reset,
         .bram_addr(addr_product), .bram_rddata(rddata_product),
         .bram_wrdata(wrdata_product), .bram_we(we_product)
     );
 
-    hadamard h0
-    #(
+    hadamard #(
         .BRAM_WIDTH(BRAM_WIDTH),
         .WORD_BYTES(WORD_BYTES),
         .ADDR_WIDTH(ADDR_WIDTH)
-    ) (
+    )
+    h0 (
         .clk, .reset,
         .ps_control, .pl_status,
         
-        .bram_addr_a(addr_a),
+        .bram_addr_a(pl_addr_a),
         .bram_rddata_a(rddata_a),
-        .bram_wrdata_a(wrdata_a),
-        .bram_we_a(we_a),
+        .bram_wrdata_a(pl_wrdata_a),
+        .bram_we_a(pl_we_a),
         
-        .bram_addr_b(addr_b),
+        .bram_addr_b(pl_addr_b),
         .bram_rddata_b(rddata_b),
-        .bram_wrdata_b(wrdata_b),
-        .bram_we_b(we_b),
+        .bram_wrdata_b(pl_wrdata_b),
+        .bram_we_b(pl_we_b),
         
-        .bram_addr_product(addr_product),
+        .bram_addr_product(pl_addr_product),
         .bram_rddata_product(rddata_product),
-        .bram_wrdata_product(wrdata_product),
-        .bram_we_product(we_product)
+        .bram_wrdata_product(pl_wrdata_product),
+        .bram_we_product(pl_we_product),
+        .mult_out_valid(mult_out_valid)
     );
+    
+    always_comb begin
+        if (mult_out_valid == 0)
+            $display("Mult_out_valid = 0");
+    end
 
     initial clk = 0;
     always #5 clk = ~clk;
 
     initial begin
+        executing = 0;
+
+        ps_wrdata_a = 0;
+        ps_wrdata_b = 0;
+        ps_wrdata_product = 0;
+
+        ps_addr_a = 0;
+        ps_addr_b = 0;
+        ps_addr_product = 0;
+        
+        ps_we_a = 0;
+        ps_we_b = 0;
+        ps_we_product = 0;
+
         ps_control = 0;
         reset = 1;
         @(posedge clk);
         @(posedge clk);
         @(posedge clk);
-        #1; reset0;
+        #1; reset = 0;
         @(posedge clk);
 
         // fill bram a0
-        for (int i=0; i < 2**ADDR_WIDTH-1; i=i+1) begin
-            wrdata_a = 2;
-            addr_a = i * 4;
-            we_a = 4'b1111;
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+//            ps_wrdata_a = 2; // integer test
+            ps_wrdata_a = 32'hc0000000; // floating point test
+            ps_addr_a = i * 4;
+            ps_we_a = 4'b1111;
             @(posedge clk);
         end
-
+        
+        ps_we_a = 0;
+        
         // fill bram b0
-        for (int i=0; i < 2**ADDR_WIDTH-1; i=i+1) begin
-            wrdata_b = 3;
-            addr_b = i * 4;
-            we_b = 4'b1111;
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+//            ps_wrdata_b = 3; // integer test
+            ps_wrdata_b = 32'h418c0000; // floating point test
+            ps_addr_b = i * 4;
+            ps_we_b = 4'b1111;
             @(posedge clk);
         end
+        
+        ps_we_b = 0;
+        
+        executing = 1;
 
+        @(posedge clk);
         ps_control[0] = 1;
         @(posedge clk);
         @(posedge clk);
@@ -102,16 +156,76 @@ module hadamard_tb
         @(posedge clk);
 
         // wait enough cycles for computations to be done
-        for (int i=0; i < 1024 + 100; i=i+1) begin
+        while (pl_status[0] == 0 || pl_status[1] == 0) begin
             @(posedge clk);
         end
-
         
-
-
-
-
-
+        executing = 0;
+        
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+            ps_addr_product = i * 4;
+            @(posedge clk);
+//            if (rddata_product != 6)
+            if (rddata_product != 32'hc20c0000)
+                $display("rddata_product[%d] = %x", i, rddata_product);
+        end
+        
+        
+        executing = 1;
+        // Start 2nd test
+        ps_control[0] = 0;
+        @(posedge clk);
+        @(posedge clk);
+        ps_control[1] = 0;
+        @(posedge clk);
+        @(posedge clk);
+        executing = 0;
+        
+        // fill bram a0
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+//            ps_wrdata_a = 2; // integer test
+            ps_wrdata_a = 32'hc0490fdb; // floating point test
+            ps_addr_a = i * 4;
+            ps_we_a = 4'b1111;
+            @(posedge clk);
+        end
+        
+        ps_we_a = 0;
+        
+        // fill bram b0
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+//            ps_wrdata_b = 3; // integer test
+            ps_wrdata_b = 32'h402df854; // floating point test
+            ps_addr_b = i * 4;
+            ps_we_b = 4'b1111;
+            @(posedge clk);
+        end
+        
+        ps_we_b = 0;
+        
+        executing = 1;
+        
+        @(posedge clk);
+        ps_control[0] = 1;
+        while (pl_status[0] == 0)
+            @(posedge clk);
+            
+        ps_control[1] = 1;
+        while (pl_status[1] == 0)
+            @(posedge clk);
+        @(posedge clk);
+        
+        executing = 0;
+        
+        for (int i=0; i < (2**ADDR_WIDTH)/WORD_BYTES; i=i+1) begin
+            ps_addr_product = i * 4;
+            @(posedge clk);
+//            if (rddata_product != 6)
+            if (rddata_product != 32'hc108a2c0)
+                $display("rddata_product[%d] = %x", i, rddata_product);
+        end
+        
+        $finish;
+        
     end
-
 endmodule
