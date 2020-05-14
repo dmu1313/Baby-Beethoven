531d53d9d7a6245cb887d183e756e1aa8e85cd2a
Daniel Mu <dmu1313@gmail.com>
2020-05-13 22:36:34 -0400

    Created new file to add hardware integration. Leave rnn.c as a pure C implementation of the neural network.

--- hardware/rnn_fpga.c
+++ hardware/rnn_fpga.c
@@ -1,3 +1,49 @@
+/******************************************************************************
+*
+* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
 
 #define HIDDEN_SIZE     512
 #define INPUT_SIZE      1572
@@ -51,6 +97,14 @@
 #include <stdio.h>
 #include <math.h>
 
+// #include "platform.h"
+// #include "xil_printf.h"
+// #include "xtmrctr.h"  // timer
+
+// #define TIMER_BASE 0x42800000   // change to match base address of your AXI timer if necessary
+// #define TIMER_FREQ 				XPAR_TMRCTR_0_CLOCK_FREQ_HZ
+// #define TMRCTR_DEVICE_ID        XPAR_TMRCTR_0_DEVICE_ID
+
 float sigmoid(float x);
 void vector_sigmoid(float * vector, int length);
 void vector_tanh(float * input, float * output, int length);
@@ -81,6 +135,7 @@ void calc_hidden_state(float * o_t, float * c_t, float * output, int length);
 
 int main()
 {
+//    init_platform();
     printf("-------------- Starting Test ------------\n\r");
 
     // Set up input one-hot vectors
@@ -234,6 +289,7 @@ int main()
     }
 
     printf("\n-------------- Done ------------\r\n\n\n\n");
+    // cleanup_platform();
     return 0;
 }
 
